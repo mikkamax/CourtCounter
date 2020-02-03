8b000c7039b4a44e2bd2b34afef8deafceaa923a
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-03 13:36:22 +0700

    Add second team score

--- app/src/main/java/com/mike/courtcounter/MainActivity.java
+++ app/src/main/java/com/mike/courtcounter/MainActivity.java
@@ -7,8 +7,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int scoreTeamA;
-    int lastInput;
+    int scoreTeamA, scoreTeamB;
+    int lastInputA, lastInputB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,31 +17,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Plus3TeamA(View v) {
-        lastInput = 3;
-        scoreTeamA += lastInput;
+        lastInputA = 3;
+        scoreTeamA += lastInputA;
         displayForTeamA();
     }
 
     public void Plus2TeamA(View v) {
-        lastInput = 2;
-        scoreTeamA += lastInput;
+        lastInputA = 2;
+        scoreTeamA += lastInputA;
         displayForTeamA();
     }
 
     public void Plus1TeamA(View v) {
-        lastInput = 1;
-        scoreTeamA += lastInput;
+        lastInputA = 1;
+        scoreTeamA += lastInputA;
         displayForTeamA();
     }
 
     public void UndoLastA(View v) {
-        scoreTeamA -= lastInput;
-        lastInput = 0;
+        scoreTeamA -= lastInputA;
+        lastInputA = 0;
         displayForTeamA();
     }
 
     public void displayForTeamA() {
         TextView scoreTeamAView = findViewById(R.id.team_a_score);
         scoreTeamAView.setText(String.valueOf(scoreTeamA));
+    }
+
+    public void Plus3TeamB(View v) {
+        lastInputB = 3;
+        scoreTeamB += lastInputB;
+        displayForTeamB();
+    }
+
+    public void Plus2TeamB(View v) {
+        lastInputB = 2;
+        scoreTeamB += lastInputB;
+        displayForTeamB();
+    }
+
+    public void Plus1TeamB(View v) {
+        lastInputB = 1;
+        scoreTeamB += lastInputB;
+        displayForTeamB();
+    }
+
+    public void UndoLastB(View v) {
+        scoreTeamB -= lastInputB;
+        lastInputB = 0;
+        displayForTeamB();
+    }
+
+    public void displayForTeamB() {
+        TextView scoreTeamAView = findViewById(R.id.team_b_score);
+        scoreTeamAView.setText(String.valueOf(scoreTeamB));
     }
 }
